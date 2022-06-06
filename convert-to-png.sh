e2b2d079205116488d94e18b89b9a1c7a016833f
osdnk <micosa97@gmail.com>
2022-06-06 12:04:04 +0200

    index.js

--- convert-to-png.sh
+++ convert-to-png.sh
@@ -4,12 +4,16 @@ then
 fi
 
 mkdir lib/pngs
+touch lib/pngs/index.js
 
 
 cd assets/originals
 
 for i in *.svg;do
     echo converting $i;
+    name=`echo $i | awk '{ print toupper( substr( $0, 1, 1 ) ) substr( $0, 2 ); }'`;
+    line=`echo export { default as ${name%.*} } from \'./${i%.*}.png\'\;`
+    echo $line >> ../../lib/pngs/index.js
     rsvg-convert -h 100 -o ${i%.*}.png $i
     mv ${i%.*}.png ../../lib/pngs
 done
@@ -19,6 +23,9 @@ cd ../../assets/overrides
 
 for i in *.svg;do
     echo converting $i;
+    name=`echo $i | awk '{ print toupper( substr( $0, 1, 1 ) ) substr( $0, 2 ); }'`;
+    line=`echo export { default as ${name%.*} } from \'./${i%.*}.png\'\;`
+    echo $line >> ../../lib/pngs/index.js
     rsvg-convert -h 100 -o ${i%.*}.png $i
     mv ${i%.*}.png ../../lib/pngs
 done
